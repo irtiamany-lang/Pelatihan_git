31534c38487895a826ace57529353a1b44e6a0e0
irtiyaah <irtiamany@gmail.com>
2026-06-23 10:12:34 +0800

    memasukkan kalimat hello

--- latihan.js
+++ latihan.js
@@ -1,1 +1,67 @@
 console.log("Hello Word")
+
+var nama ="Irtiyaah"
+var NomorUrut = 12
+
+console.log("nama")
+console.log(nama)
+console.log(NomorUrut)
+
+var open = true
+if(open == true){
+    console.log ("BUKA")
+
+}
+
+
+// operasi aritmatika
+var a = 5
+var b = 5
+var tambah = a + b
+var kurang = a - b
+var kali = a * b
+var bagi = a / b
+var modulus = a % b
+
+console.log("Hasil tambah =",  a + b)
+console.log("Hasil kurang =", a - b)
+console.log("Hasil kali =", a * b)
+console.log("Hasil bagi =", a / b)
+console.log("Hasil modulus =", a % b)
+
+// Operator Perbandingan
+console.log("Operator Perbandingan")
+console.log("====================")
+
+console.log(1 != 1)
+console.log(1 == "1")
+console.log(1 === "1")
+console.log(1 == 1)
+console.log(1 === 1)
+console.log("1" == "1")
+console.log("1" === "1")
+console.log(1 > 2)
+console.log(1 < 2)
+console.log(1 <= 1)
+console.log(1 >= 1)
+console.log(2 <= 4)
+console.log(5 >= 1)
+
+//Operator Kondisional ()
+// &&(and) ||(or)
+console.log("Operator Kondisional")
+console.log(false || false)
+console.log(false && true)
+
+var status = "close"
+var jamBuka = 9
+console.log(status = "open" || jamBuka >= 9)
+
+var status = "open"
+var jamBuka = 9
+console.log(status = "open" && jamBuka <= 8)
+
+
+
+
+ 
