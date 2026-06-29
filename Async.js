1b57d99c15b5bae8709a12beec25e9bbb2a08d9c
irtiyaah <irtiamany@gmail.com>
2026-06-29 08:50:35 +0800

    Async

--- Async.js
+++ Async.js
@@ -5,6 +5,9 @@
 //         }else{
 //             reject("gagal")
 //         }
+
+
+
         
 //     })
 
@@ -127,58 +130,33 @@
 
 // hello();
 
-function periksaDataPasien(nomorIdPasien) {
-    var dataPasien = [
-        {id: 1, nama: "Jhone", jenisKelamin: "Laki-Laki"},
-        {id: 2, nama: "Daffa", jenisKelamin: "Perempuan"},
-        {id: 3, nama: "Yulita", jenisKelamin: "Perempuan"},
-        {id: 4, nama: "Irtiyaah", jenisKelamin: "Perempuan"},
-    ]
-    return new Promise(function (resolve, reject){
-        var pasien = dataPasien.find(x => x.id === nomorIdPasien)
-        if (pasien === undefined){
-            reject("data pasien tidak ada")
-        }else{
-            resolve(pasien)
-        }
-    })
+
+// function doAsync(nomorIdPasien) {
+//     return new Promise (function (resolve, reject){
+//         var dataPasien = [
+//             {id : 1, mana: "Jhone", jenisKelamin : "Laki-laki"},
+//             {id : 2, mana: "Daffa", jenisKelamin : "Perempuan"},
+//             {id : 3, mana: "Andra", jenisKelamin : "Laki-laki"},
+//             {id : 4, mana: "Ilham", jenisKelamin : "Laki-laki"},
+//         ]
+//         var pasien = dataPasien.find(x=> x.id === nomorIdPasien)
+//         if (pasien ===  undefined){
+//             reject("data pasien tidak ada")
+//         }else {
+//             resolve(pasien)
+//         }
+
+//     })
+// }
+// async function hello(nomorIdPasien) {
+//     try{
+//         var result = await doAsync(nomorIdPasien)
+//         console.log(result);
+//        }catch(err){
+//         console.log(err);
     
-    
-}
-periksaDataPasien(5).then(function(data){
-    console.log(data)
-}).catch(function(err){
-    console.log(err)
-})
-function doAsync() {
-    return new Promise(function(resolve, reject) {
-        var check = true;
-        if (check) {
-            resolve("berhasil");
-        } else {
-            reject("gagal");
-        }
-    })
-}
+//     }
+// }
 
-async function hello() {
-    try {
-        var result = await doAsync();
-        console.log("// Async/await");
-        console.log(result);
-    } catch(err) {
-        console.log("// Async/await");
-        console.log(err);
-    }
-}
+// hello(2)
 
-async function memeriksaPasien(nomorIdPasien) {
-    try {
-        var hasil = await periksaDataPasien(nomorIdPasien);
-        console.log(hasil);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-hello();
