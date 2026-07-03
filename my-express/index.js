69e2d04134f47162591c3db2998ab0dd7fd26451
irtiyaah <irtiamany@gmail.com>
2026-07-03 13:34:19 +0800

    git

--- my-express/index.js
+++ my-express/index.js
@@ -3,86 +3,120 @@ const app = express();
 const port = 3000;
 
 let movies = [
-    {id: 1, title: "Spider-Man", year: 2001},
-    {id: 1, title: "John Wick", year: 2014},
-    {id: 1, title: "The Avengers", year: 2012},
-    {id: 1, title: "Logan", year: 2017}
+    {id: 1, title: "spiderman", year: 2002},
+    {id: 2, title: "Jhon wick", year: 2014},
+    {id: 3, title: "The Avegers", year: 2012},
+    {id: 4, title: "Logan", year: 2017}
 ]
 
-const getMovies = (req, res) => {
-    let {title} = req.query
-    
-    
-    if(title == undefined){
-        title = ""
-    }
-console.log(title)
-    let result = ""
+// const getMovies = (req, res) => {
+//   let {title} = req.query 
 
-    movies.forEach((item, index) => {
-        if(item.title.toLowerCase().includes(title)){
-            result += `<H1>${index + 1}. <br>tittle : ${item.title} : ${item.year}`
-        }
-})
+//    console.log(title)
 
-        if (result === ""){
-        res.send(`<h1>Data tidak ditemukan.<hbr>`)
-        }
-        res.send(result);
-   
-  } 
-    
-const getMoviesbyId = (req, res) => {
-  let {id} = req.params;
-  let hasil = movies.find((item) => {
-    return item.id === Number(id)
-  })
+//    if(title == undefined){
+//    title = ""
+//    }
 
-res.send(`${hasil.id}`)
-}
+//    let result = ""
+
+//    movies.forEach((item, index) => {
+//     if(item.title.toLowerCase().includes(title)) {
+//     result += `<h3> ${index+1}.</br>tittle : ${item.title}<br>year : ${item.year}`
+//     }
+//     // if (result === "")
+
+//     // res.send(`<h3>Data tidak ditemukan.<hbr>`)
+//   })
+//   res.send(result);
+// }
+
+
+// const getMoviesId = (req, res) => {
+//   let {id} = req.params;
+//   let hasil = movies.find((item) => {
+//     return item.id === Number(id)
+//   })
+// // if(hasil){
+// //   return res.send("<h3>Movie tidak dapat ditemukan</br>")
+// // }
+//   res.send(`<h3>Title: ${hasil.title} <br> Year: ${hasil.year}</br>`)
+// }
+
+// app.get('/movies', getMovies);
+// app.get('/movies/:id', getMoviesId);
+
+
+
 
 
 const getMoviesApi = (req, res) => {
-    let {title} = req.query
-    console.log(title)
-    if(title == undefined){
-        title = ""
-    }
-    let result = movies.filter((item, index) =>{
-      return item.title.toLowerCase().includes(title.toLowerCase())
-    })
-       res.json(result)
-   
-  } 
-    
-const getMoviesbyId = (req, res) => {
-  let {id} = req.params;
-  let hasil = movies.find((item) => {
-    return item.id === Number(id)
-  })
+  let {title, year} = req.query 
+  // console.log(title)
 
-res.send(`<h3>Title: ${hasil.title} <br> Year: ${hasil.year}</br>`)
+  if(title == undefined){
+   title = ""
+  }
+
+  // let {id, title} = req.params;
+   let result = movies.filter((item, index) => {
+    return (item.title.toLocaleLowerCase().includes(title.toLocaleLowerCase()) && (!year || item.year == year)
+   )
+  })
+  if(!result){
+    res.json("<h3>Movie tidak dapat ditemukan</br>")
+  }
+   res.json(result)
+  }
+
+  
+//   
+// }
+
+const getMoviesbyIdApi = (req, res) => {
+let {id} = req.params;
+let hasil = movies.find((item) => {
+  return item.id === Number(id)
+})
+res.json(hasil)
+}
+
+const loggerMiddleware = (req, res, next) => {
+  console.log("Ada request masuk")
+  next() 
+}
+
+const tokenMiddleware = (req, res, next) => {
+  let {token} = req.query
+
+  if (token === "12345"){
+    next()
+  } else {
+    res.status(401).json({
+      message: "Token tidak valid"
+    })
+  }
+}
+
+const checkMovieIdMiddleware = (req, res, next) => {
+  let { id } = req.params
+  let result = movies.find(item => item.id === Number(id))
+  if (result) {
+    next()
+  } else {
+    res.status(404).json({ message: "Movie tidak ditemukan"})
+  }
+}
+
+const timeMiddleware = (req, res, next) => {
+  console.log(new Date());
+  next()
 }
 
 
 
-app.get('/movies', getMovies);
-app.get('/moviesbyId', getMoviesbyId);
-
-app.get('api/movies', getMovies);
-app.get('/movies/:id', getMoviesbyId);
-
-
-    
-     
-
-
-
-
-
-
-
-
+app.get('/api/movies', loggerMiddleware, timeMiddleware, tokenMiddleware, getMoviesApi)
+app.get('/api/movies/:id',loggerMiddleware, checkMovieIdMiddleware, getMoviesbyIdApi)
 
 
 app.get('/', (req, res) => {
